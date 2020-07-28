47a285f47a74a7b65807e70ce280e8675e557ebe
Bhupesh-V <varshneybhupesh@gmail.com>
2020-07-28 15:39:02 +0530

    automate README

---
+++ update_readme.py
@@ -0,0 +1,84 @@
+import sys
+
+HEADER = """
+<img align="left" src="https://gist.github.com/Bhupesh-V/0246a3f681d2533d21efb1206d1ba9d4/raw/af7d53bfdbf30f725ef7ade206200086820739fd/AboutMe.gif" height="180px"> 
+<div>
+  <h3 align="right">Helloo :wave: <i>GitHubers</i> :relaxed:</h3>
+  <p align="right">I'm a Backend Developer and FOSS Enthusiast. I got interested in programming when I learned how a <code>for</code> loop works 😮 in 2016 and since then I got interested in CS and have never looked back. Right now I write (and work) with Python, Go, C++, Bash &amp; Javascript (in that order). I am a side-project guy, and love 💜 to be a part of programming &amp; Open-Source communities.</p>
+</div>
+
+
+---
+### ⚽ My Goals 
+
+- [ ] Launch 🚀 a SaaS
+- [ ] Get a Laptop 💻
+- [ ] Contribute to 2-3 Big FOSS Projects 🙈 (I'm lazy)
+- [ ] Help 100+ beginners or codenewbies to contribute to OpenSource.
+- [ ] Start A Newsletter 🗞️ for my [blog](https://bhupesh.codes).
+- [ ] Write more stuff to share my experiences 🤔 & learning (hopefully on freeCodeCamp & HackerNoon)
+
+
+### Recent Works
+<details>
+  <summary>🌟 Projects 🌟</summary>
+  <table>
+    <tr>
+      <td><samp><h4 align="center">dotman</h4></samp>
+        <p align="center">
+        <img align="center" src="https://github.com/Bhupesh-V/dotman/blob/master/assets/dotman-logo.png" width="200">
+        </p>
+        <a align="center" href="https://github.com/Bhupesh-V/dotman"><b> dotman is a simple &amp; elegant dotfiles manager</b></a>
+      </td>
+      <td><samp><h4 align="center">defe</h4></samp>
+        <p align="center">
+        <img align="center" src="https://raw.githubusercontent.com/Bhupesh-V/defe/2836e20d0416a4232e7d7f81a7988250e1d6718d/static/images/logodefe.svg" width="100">
+        </p>
+        <a align="center" href="https://github.com/Bhupesh-V/defe"><b> A tech feed aggregator for Developers</b></a>
+      </td>
+      <td><samp><h4 align="center">Memer Action</h4></samp>
+        <p align="center">
+           <img align="center" src="https://github.com/Bhupesh-V/memer-action/blob/master/images/header.png?raw=true" width="178">
+        </p>
+        <a align="center" href="https://github.com/Bhupesh-V/memer-action"><b>A GitHub Action for Programmer Memes xD</b></a>
+      </td>
+    </tr>
+  </table>
+</details>
+
+"""
+FOOTER = """
+### What is Bhupesh upto nowadays 👀
+
+- 🌱 I’m currently learning ...
+   - Go (Golang)
+- 🔭 I’m currently working on ...
+    - All of my pinned projects below 👇 (& more hidden ones)
+
+### What else ❔
+
+- 😄 Pronouns: (He/Him)
+- ⚡ Fun fact: I make & share memes, want to see some ?
+- Also I am looking for some remote job/contract work opportunities (going to graduate in 2020 hopefully)
+
+---
+"""
+
+
+def main(todays_meme):
+
+    with open('README.md', 'w') as file:
+        file.write(HEADER)
+        file.write("### Today's Meme \\( ﾟヮﾟ)/\n\n")
+        file.write(
+            "<details><summary>{0}</summary>\n\n".format(todays_meme[1]))
+        file.write("""> **{title}**\n![{title}]({meme})\n<sub>ℹ️ <a href="{source}">Source</a> [ Powered By 🔥 <a href="https://github.com/Bhupesh-V/memer-action">Memer Action</a> ]</sub>""".
+                   format(title=todays_meme[1], meme=todays_meme[0], source=todays_meme[2]))
+        file.write("\n</details>\n")
+        file.write(FOOTER)
+
+
+if __name__ == '__main__':
+    # Args: [meme, title, source]
+    # Run: python update_readme.py meme title source
+    main(sys.argv[1:])
