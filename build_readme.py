a1693399b3f38e431919ba38f174db466586bc19
Bhupesh Varshney <varshneybhupesh@gmail.com>
2023-02-02 17:11:32 +0530

    update bio

--- build_readme.py
+++ build_readme.py
@@ -18,11 +18,12 @@ NO_OF_BLOGS = NO_OF_TILS = 4
 HEADER = """
 <div>
   <h3>wassup nerds 🖖</h3>
-  <p>Developer, FOSS Enthusiast & Creator living in New Delhi, India. I like to build & create stuff using Python, Go & Bash. I make Web, Tools, and Open-Source & frequently write about tech & my journey on my blog/newsletter.
+  <p>Software Craftsman & Tech Writer living in New Delhi, India. I like to build stuff using Python, Go & Bash. I make Web, Tools, and Open-Source & frequently write about tech on my blog/newsletter.</p>
   <ul>
     <li>I am a Community Admin at <a href="https://reddit.com/r/developersIndia">r/developersIndia</a> (Join us 🚀)</li>
     <li>In my free time, I am probably sleeping or reading something online.</li>
     <li>I write a lot of technical content, you can witness some of that below 👇</li>
+    <li>Currently Learning <code>Elixir</code></li>
   </ul>
 </div>
 
